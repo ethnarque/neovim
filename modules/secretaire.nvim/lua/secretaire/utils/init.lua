db1aa28122154c1d7818ac2702b9b4539371cdd7
pml <42704376+pmlogist@users.noreply.github.com>
2023-11-08 22:34:31 +0100

    Add new autocmds
    WIP

--- modules/secretaire.nvim/lua/secretaire/utils/init.lua
+++ modules/secretaire.nvim/lua/secretaire/utils/init.lua
@@ -5,4 +5,8 @@ function M.lazy_load(pkg_name)
     vim.cmd(string.format("packadd %s", pkg_name))
 end
 
+function M.augroup(name)
+    return vim.api.nvim_create_augroup("secretaire-" .. name, { clear = true })
+end
+
 return M
