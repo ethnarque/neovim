16781e45ca159348fcd9fc49ee31d853b0d5a6dd
pml <42704376+pmlogist@users.noreply.github.com>
2023-12-28 23:47:21 +0100

    Add auto dark mode

---
+++ packages/auto-dark-mode.nix
@@ -0,0 +1,11 @@
+{ pkgs }: pkgs.vimUtils.buildVimPlugin {
+  pname = "auto-dark-mode.nvim";
+  version = "0.10.0";
+  src = pkgs.fetchFromGitHub {
+    owner = "f-person";
+    repo = "auto-dark-mode.nvim";
+    rev = "76e8d40d1e1544bae430f739d827391cbcb42fcc";
+    hash = "sha256-uJ4LxczgWl4aQCFuG4cR+2zwhNo7HB6R7ZPTdgjvyfY=";
+  };
+  meta.homepage = " https://github.com/f-person/auto-dark-mode.nvim";
+}
