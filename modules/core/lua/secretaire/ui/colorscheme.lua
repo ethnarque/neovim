7fc179dc4adb0ca67d5fee1c5f9e6b1a513a8143
pml <42704376+pmlogist@users.noreply.github.com>
2024-01-26 16:20:43 +0100

    Fix colorscheme for linux servers

--- modules/core/lua/secretaire/ui/colorscheme.lua
+++ modules/core/lua/secretaire/ui/colorscheme.lua
@@ -1,31 +1,47 @@
 _G.Secretaire.ui = {}
 
-vim.cmd([[ colorscheme rose-pine-main ]])
+---@type "Linux" | "Darwin"
+local system
 
-require("auto-dark-mode").setup({
-	update_interval = 1000,
-	set_dark_mode = function()
-		require("rose-pine").setup({
-			groups = {
-				background = "#101010",
-			},
-			highlight_groups = {
-				WinSeparator = { fg = "#1A1A1A" },
-				EndOfBuffer = { fg = "#101010" },
-			},
-		})
-		vim.cmd("colorscheme rose-pine-main")
-	end,
-	set_light_mode = function()
-		require("rose-pine").setup({
-			groups = {
-				background = "base",
-			},
-			highlight_groups = {
-				WinSeparator = { fg = "base" },
-				EndOfBuffer = { fg = "base" },
-			},
-		})
-		vim.cmd("colorscheme rose-pine-dawn")
-	end,
-})
+system = vim.loop.os_uname().sysname
+
+if system == "Linux" and os.getenv("XDG_CURRENT_DESKTOP") == "" then
+	require("rose-pine").setup({
+		groups = {
+			background = "#101010",
+		},
+		highlight_groups = {
+			WinSeparator = { fg = "#1A1A1A" },
+			EndOfBuffer = { fg = "#101010" },
+		},
+	})
+	vim.cmd("colorscheme rose-pine-main")
+else
+	require("auto-dark-mode").setup({
+		update_interval = 1000,
+		set_dark_mode = function()
+			require("rose-pine").setup({
+				groups = {
+					background = "#101010",
+				},
+				highlight_groups = {
+					WinSeparator = { fg = "#1A1A1A" },
+					EndOfBuffer = { fg = "#101010" },
+				},
+			})
+			vim.cmd("colorscheme rose-pine-main")
+		end,
+		set_light_mode = function()
+			require("rose-pine").setup({
+				groups = {
+					background = "base",
+				},
+				highlight_groups = {
+					WinSeparator = { fg = "base" },
+					EndOfBuffer = { fg = "base" },
+				},
+			})
+			vim.cmd("colorscheme rose-pine-dawn")
+		end,
+	})
+end
