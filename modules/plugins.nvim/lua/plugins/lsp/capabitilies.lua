f4b2a3e41457d3799420b361cab6edb3767b2844
pml <42704376+pmlogist@users.noreply.github.com>
2024-01-27 00:50:34 +0100

    Remove deprecated plugin module

--- modules/plugins.nvim/lua/plugins/lsp/capabitilies.lua
+++
@@ -1,8 +0,0 @@
-local util = require("core.util")
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-util.lazy_load("cmp-nvim-lsp")
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-return capabilities
